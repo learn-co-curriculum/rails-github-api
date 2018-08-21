9ba0c554fc6b1ccc60fea1aaedef205e8888b38c
maxwellbenton <maxwellbenton@gail.com>
2018-08-21 10:38:26 -0400

    rebase from master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user
 
+  before_action :authenticate_user
+
   private
 
-    def authenticate_user
-      redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
-    end
+  def authenticate_user
+    redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" unless logged_in?
+  end
 
-    def logged_in?
-      !!session[:token]
-    end
+  def logged_in?
+    !!session[:token]
+  end
 end
